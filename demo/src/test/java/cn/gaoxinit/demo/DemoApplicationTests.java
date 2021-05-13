2c347aef3c6cdb714e16ddd41edcaf8d888135af
gaoxin29 <2362634990@qq.com>
2021-05-13 11:53:58 +0800

    提交测试

---
+++ demo/src/test/java/cn/gaoxinit/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.gaoxinit.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
