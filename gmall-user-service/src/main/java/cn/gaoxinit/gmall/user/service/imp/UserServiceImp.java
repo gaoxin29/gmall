89ce7ab2ec6452823016bf8a00250688458e9826
xiaoxinit <2362634990@qq.com>
2021-12-05 21:38:23 +0800

    15151515

--- gmall-user-service/src/main/java/cn/gaoxinit/gmall/user/service/imp/UserServiceImp.java
+++ gmall-user-service/src/main/java/cn/gaoxinit/gmall/user/service/imp/UserServiceImp.java
@@ -43,6 +43,6 @@ public class UserServiceImp implements UserService {
     }
 
     public static void main(String[] args) {
-        System.out.println("再次修改看能否成功上传 14141414");
+        System.out.println("再次修改看能否成功上传 15151515");
     }
 }
