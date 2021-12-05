8c503c2aaf0ec44c1d862341a3c54667d0a3956b
xiaoxinit <2362634990@qq.com>
2021-12-05 20:22:52 +0800

    再次修改一下 看能否还能成功上传

--- gmall-user-service/src/main/java/cn/gaoxinit/gmall/user/service/imp/UserServiceImp.java
+++ gmall-user-service/src/main/java/cn/gaoxinit/gmall/user/service/imp/UserServiceImp.java
@@ -43,6 +43,6 @@ public class UserServiceImp implements UserService {
     }
 
     public static void main(String[] args) {
-        System.out.println("再次测试再次测试再次测试");
+        System.out.println("再次修改看能否成功上传");
     }
 }
